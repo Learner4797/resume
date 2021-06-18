a36d72d7230b94de2bbcd0368cfb6c05bccad7e3
Ashish singh <65036327+Learner4797@users.noreply.github.com>
2021-06-18 16:11:10 +0530

    Add files via upload

---
+++ portfolio1/portfolio.js
@@ -0,0 +1,40 @@
+
+
+      // // Show active menu when scrolling
+      // const highlightMenu = () => {
+      // const elem = document.querySelector('.highlight');
+      // const homeMenu = document.querySelector('#home-page');
+      // const aboutMenu = document.querySelector('#about-page');
+      // const skillsMenu = document.querySelector('#skills-page');
+      // const projectMenu = document.querySelector('#project-page');
+      // let scrollPos = window.scrollY;
+      // // console.log(scrollPos);
+
+      // // adds 'highlight' class to my menu items
+      // if (window.innerWidth > 960 && scrollPos < 590) {
+      // homeMenu.classList.add('highlight');
+      // aboutMenu.classList.remove('highlight');
+      // return;
+      // } else if (window.innerWidth > 960 && scrollPos < 1400) {
+      // aboutMenu.classList.add('highlight');
+      // homeMenu.classList.remove('highlight');
+      // skillsMenu.classList.remove('highlight');
+      // return;
+      // } else if (window.innerWidth > 960 && scrollPos < 2345) {
+      // skillsMenu.classList.add('highlight');
+      // aboutMenu.classList.remove('highlight');
+      // projectMenu.classList.remove('highlight');
+      // return;
+      // } else if (window.innerWidth > 960 && scrollPos < 5345) {
+      // projectMenu.classList.add('highlight');
+      // skillsMenu.classList.remove('highlight');
+      // return;
+      // }
+
+      // if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+      // elem.classList.remove('highlight');
+      // }
+      // };
+
+      // window.addEventListener('scroll', highlightMenu);
+      // window.addEventListener('click', highlightMenu);
